c581de122fb0a6ecc2a42b812e9ae36de226a6ca
paulobressan <paulo.bressan@outlook.com>
2025-07-04 17:08:02 -0300

    feat: updated docker compose

--- examples/vending-machine/web/vite.config.ts
+++ examples/vending-machine/web/vite.config.ts
@@ -3,7 +3,12 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-  plugins: [wasm(), tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [wasm(), tailwindcss(), reactRouter(), tsconfigPaths(), topLevelAwait()],
+  assetsInclude: ['**/*.wasm'],
+  build: {
+    target: 'esnext',
+  }
 });
